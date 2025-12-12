a7dc83c7bfd79f0c953515776d2baa563a1e88b1
AmazingMuffin <40176470+AmazingMuffin@users.noreply.github.com>
2025-12-12 15:00:57 +0000

    modifier page RealEstate

--- caarbon-real-estate-client/src/app/realestate/page.tsx
+++ caarbon-real-estate-client/src/app/realestate/page.tsx
@@ -3,7 +3,7 @@ export default function RealEstateList() {
 
     return(
         <div>
-            A faire : récupérer la liste des propriétés tokenizables
+            A faire : récupérer la liste des propriétés tokenizables test
         </div>
     )
 
